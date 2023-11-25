f16f50f0705ed8424676987a0e87f480db334662
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-11-25 16:45:56 +0200

    Move express configs in modules

---
+++ src/config/expressConfig.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+function expressConfig(app) {
+    //Express configs
+    app.use(express.static(path.resolve(__dirname, '../public')));
+    //app.use(express.static('src/public));
+
+}
+
+module.exports = expressConfig;
