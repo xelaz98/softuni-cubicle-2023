db4bce73790f146820c4fa40cb957f6a1ccbb3b8
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-11-26 13:45:05 +0200

    express config

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -4,6 +4,7 @@ const path = require('path');
 function expressConfig(app) {
     //Express configs
     app.use(express.static(path.resolve(__dirname, '../public')));
+    app.use(express.urlencoded({extended: false}));
     //app.use(express.static('src/public));
 
 }
