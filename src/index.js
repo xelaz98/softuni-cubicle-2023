f875e99a3da05f333cf3cf4cfb91ac06736a9df3
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-11-29 16:51:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ expressConfig(app);
 handlebarsConfig(app);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index'); 
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
