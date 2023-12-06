b830940b0602c93d0ec5775215f710510f36fa7c
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-12-06 10:50:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,4 +14,4 @@ handlebarsConfig(app);
 
 app.get('/', homeController.getHome);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));  
