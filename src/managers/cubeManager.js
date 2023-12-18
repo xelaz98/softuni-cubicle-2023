cbc5316b6125b62968d54fa9479e55473f866feb
AlexP <68079031+xelaz98@users.noreply.github.com>
2023-12-18 12:22:46 +0200

    Add cubeManager with create logic

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,5 +1,15 @@
 const cubes = [];
 
+exports.getAll = () => cubes.slice();
+
 exports.create = (cubeData) => {
-    cubes.push(cubeData);
-}  
+ 
+    const newCube = {
+        id: cubes.length + 1,
+        ...cubeData,
+    };
+
+    cubes.push(newCube);
+
+    return newCube;
+}; 
